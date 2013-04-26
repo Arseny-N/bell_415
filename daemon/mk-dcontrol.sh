ec3ba01115b89a091a5cbf5d45ecdf8be3eec9cb
VskyNeri <arseny-n@yandex.ru>
2013-04-27 01:00:51 +0400

    bug-fix

--- daemon/mk-dcontrol.sh
+++ daemon/mk-dcontrol.sh
@@ -1,32 +1,35 @@
 #!/bin/sh
 
-BIN_DIR=$1
-LOG_DIR=$2
-PID_FILE=$3
-
 echo '#!/bin/bash'
 
-echo 'logdir='$LOG_DIR
-echo 'pid_file='$PID_FILE
-echo 'bin_dir='$BIN_DIR
-
+echo 'bin_dir='$1
+echo 'ring_prog='$2
 echo '
 case "$1" in
-    "kill" )
-	kill -s SIGTERM $( cat "$pid_file")
-	;;
-    "is-up")
-	kill -0 $( cat "$pid_file" ) 2&> /dev/null && echo "runnung" || echo "not runnuing"
-	;;
-    "rexec")
-	kill -s SIGHUP $( cat "$pid_file" )
-	;;
-    "logs")
-        echo "$logdir"
-        ;;
-    "up")
-	$bin_dir/run --dump_config  --log_err $logdir/run-err.log --log_out $logdir/run-dbg.log --ring_prog $bin_dir/ringer --pid_file $pid_file
-	;;
+	"kill" )
+		kill -s SIGTERM $( cat "$pid_file")
+		;;
+	"state")
+		kill -0 $( cat "$pid_file" ) 2&> /dev/null && echo "up" || echo "not runnuing"
+		;;
+	"rexec")
+		kill -s SIGHUP $( cat "$pid_file" )
+		;;
+	"logs")
+	        echo "$logdir"
+	        ;;
+	"up")
+		$bin_dir/run $@
+		;;
+	"ring")
+		$ring_prog $@		
+		;;
+	"ring1")
+		$ring_prog $@		
+		;;
+	"ring2")
+		$ring_prog $@		
+		;;
     * )
 	echo "Unknown action $1"
 	;;
